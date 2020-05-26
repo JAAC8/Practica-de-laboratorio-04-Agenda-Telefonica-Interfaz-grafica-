27e9138b322a2f8a2489ed74ad79986f475d4a0c
José Andrés Abad. <64880801+JAAC8@users.noreply.github.com>
2020-05-26 14:43:01 -0500

    Actualización

--- src/main/java/ec/edu/ups/gui/MenuUsuario.java
+++ src/main/java/ec/edu/ups/gui/MenuUsuario.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package GUI;
+package ec.edu.ups.gui;
 
 import java.awt.Frame;
 import java.awt.Label;
